1b58fb41cf42096790799e4f19c335a7c3b1a782
T. Allen Harding <softwareisharding@gmail.com>
2022-03-09 16:56:11 -0800

    housekeeping inside the server

--- dist/server/server.js
+++ dist/server/server.js
@@ -2,23 +2,43 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import http from 'http';
-import { Server } from 'socket.io';
+import * as socketIO from 'socket.io';
 const PORT = process.env.PORT || 29070;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const distPath = path.dirname(__dirname);
-//(3/9/22) cry havoc and let slip the dogs of server init functions
-const expressApp = express();
-const server = http.createServer(expressApp);
-const io = new Server(server);
-expressApp.use(express.static(path.join(distPath, 'public')));
-server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
-io.on('connection', (socket) => {
-    console.log('client connection: ' + socket.id);
-    socket.on('hello', (msg) => {
-        console.log(`${socket.id} sent ${msg}`);
-    });
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`);
-    });
-});
+// //(3/9/22) cry havoc and let slip the dogs of server init functions
+// const expressApp = express();
+// const server: http.Server = http.createServer(expressApp);
+// const io: socketIO.Server = new socketIO.Server(server);
+// expressApp.use(express.static(path.join(distPath, 'public')));
+// server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+// io.on('connection', (socket: socketIO.Socket) => {
+//     console.log('client connection: ' + socket.id);
+//     socket.on('hello', (msg) => {
+//         console.log(`${socket.id} sent ${msg}`);
+//     });
+//     socket.on('disconnect', () => {
+//         console.log(`${socket.id} disconnected`);
+//     });
+// });
+class GremlinServer {
+    constructor(port) {
+        this.port = process.env.PORT || port;
+        const expressApp = express();
+        expressApp.use(express.static(path.join(distPath, 'public')));
+        this.server = new http.Server(expressApp);
+        this.io = new socketIO.Server(this.server);
+        this.io.on('connection', (socket) => {
+            console.log(`client connection: ${socket.id}`);
+            socket.on('disconnect', () => {
+                console.log(`client disconnect: ${socket.id}`);
+            });
+        });
+    }
+    start() {
+        this.server.listen(this.port);
+        console.log(`server listening on port ${this.port}`);
+    }
+}
+new GremlinServer(29070).start();
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,39 +3,39 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import http from 'http';
 
-import { Server } from 'socket.io';
+import * as socketIO from 'socket.io';
 
-
-
-const PORT = process.env.PORT || 29070;
 
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
 const distPath: string = path.dirname(__dirname);
 
+class GremlinServer {
+    private server: http.Server;
+    private port: string | number;
+    private io: socketIO.Server;
 
+    constructor(port: number) {
+        this.port = process.env.PORT || port; 
+        const expressApp = express();
+        expressApp.use(express.static(path.join(distPath, 'public')));
 
+        this.server = new http.Server(expressApp);
+        this.io = new socketIO.Server(this.server);
 
-//(3/9/22) cry havoc and let slip the dogs of server init functions
-const expressApp = express();
-const server: http.Server = http.createServer(expressApp);
-const io: Server = new Server(server);
+        this.io.on('connection', (socket: socketIO.Socket) => {
+            console.log(`client connection: ${socket.id}`);
 
+            socket.on('disconnect', () => {
+                console.log(`client disconnect: ${socket.id}`);
+            });
+        });
+    }
 
-expressApp.use(express.static(path.join(distPath, 'public')));
+    public start() {
+        this.server.listen(this.port);
+        console.log(`server listening on port ${this.port}`);
+    }
+}
 
-server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
-
-io.on('connection', (socket) => {
-    console.log('client connection: ' + socket.id);
-
-    socket.on('hello', (msg) => {
-        console.log(`${socket.id} sent ${msg}`);
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`);
-    });
-
-
-})
+new GremlinServer(29070).start();
